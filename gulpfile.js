30b846264a99b85f8b7de410d9a915b320d17975
iArt0s <oarthemlive@gmail.com>
2018-12-14 21:47:47 +0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,11 @@ const rename = require('gulp-rename');
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 
+const ghpages = require('gh-pages');
+const path = require('path');
+
+
+
 function styles() {
   return src(`${dir.src}scss/style.scss`)
     .pipe(plumber())
@@ -96,6 +101,11 @@ function clean() {
 }
 
 exports.clean = clean;
+
+function deploy(cb) {
+   ghpages.publish(path.join(process.cwd(), dir.build), cb);
+}
+exports.deploy = deploy;
 
 function serve() {
   browserSync.init({
