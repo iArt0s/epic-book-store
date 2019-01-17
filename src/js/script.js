a8e547f9db4dd2a2f0e691ac5aca6c2ac3d35bd7
iArt0s <oarthemlive@gmail.com>
2019-01-17 17:43:30 +0300

    nm--no-verify

--- src/js/script.js
+++ src/js/script.js
@@ -41,22 +41,46 @@ var carousel = new Swiper ('.swiper-container', {
 
 
 
-// Функция ymaps.ready() будет вызвана, когда
-    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-    ymaps.ready(init);
-    function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [55.76, 37.64],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 7
-        });
-    }
+ymaps.ready(function () {
+  let myMap = new ymaps.Map('map', {
+    center: [59.916933, 30.280519],
+    zoom: 17,
+    controls: [],
+  },
+  {
+    suppressMapOpenBlock: true,
+    searchControlProvider: 'yandex#search'
+  }),
+
+  // Создаём макет содержимого.
+  MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+  ),
+
+  myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'Собственный значок метки',
+  }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: '../img/icon.png',
+      // Размеры метки.
+      iconImageSize: [38,49],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-5, -38]
+  });
+
+  myMap.behaviors.disable('scrollZoom'),
+  myMap.geoObjects.add(myPlacemark);
+
+  myMap.controls.add('zoomControl', {
+    size: "large"
+  });
+});
+
+
 
 
 
